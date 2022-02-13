d4ecfd3eeb62c53c2acac19065c2f1409bfd00ab
Kassie Becker <kassaundralee@hotmail.com>
2022-02-13 15:02:21 -0600

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solutions in this file
+const names = ["Guadalupe", "Ollie", "Aki"];
+
+function writeCards(names, holiday) {
+    let message = []
+    for (let i=0; i < names.length; i++) {
+        message.push(`Thank you, ${names[i]}, for the wonderful ${holiday} gift!`)
+        debugger;
+    }    
+    return message
+}
+
+writeCards(names, "surprise");
+
+
+
+
+function countDown(thisNumber) {
+    while (thisNumber >= 0) {
+    console.log(thisNumber--);
+    }
+}
+
+countDown(4)
